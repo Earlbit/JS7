4061ef0409f2c123cab412678d15df8e4c3c0ab2
Earl Bit <earlbit247@gmail.com>
2023-06-04 21:33:36 +0200

    JS7

---
+++ main.js
@@ -0,0 +1,10 @@
+let firstNumber = 5;
+let secondNumber = 10;
+
+function sumNumbers(a, b) {
+    return a + b;
+}
+
+let result = sumNumbers(firstNumber, secondNumber);
+console.log("The sum is: " + result);
+
